30ae12422e574aeb2bd1e479444916f55ebb5c18
Robert Coha <robertcohaflorez@gmail.com>
2026-05-12 11:57:30 -0500

    estructura base de módulos del proyecto

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('¡Hola desde el backend!');
-});
+}); 
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-}); 
+});  
